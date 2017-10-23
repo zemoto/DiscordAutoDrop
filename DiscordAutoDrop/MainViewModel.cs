b29941e8f28dc42253a9937e49c639b9a07ea25a
David Markachev <zemoto@gmail.com>
2017-10-23 12:25:30 -0400

    +MainViewModel to use as MainWindow data context

---
+++ DiscordAutoDrop/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using DiscordAutoDrop.MVVM;
+using DiscordAutoDrop.Utilities;
+using FlaUI.Core.AutomationElements.Infrastructure;
+using FlaUI.Core.Patterns;
+
+namespace DiscordAutoDrop
+{
+   internal sealed class MainViewModel : ViewModelBase
+   {
+      private readonly AutomationElement _discord;
+      private readonly IInvokePattern _messageBoxLegacyPattern;
+
+      public MainViewModel()
+      {
+         using ( var discordFinder = new DiscordFinder() )
+         {
+            Debug.Assert( discordFinder.Inititialize() );
+            (_discord, _messageBoxLegacyPattern) = discordFinder.FindDiscord();
+         }
+      }
+
+      private RelayCommand _buttonPressedCommand;
+      public RelayCommand ButtonPressedCommand => _buttonPressedCommand ?? ( _buttonPressedCommand = new RelayCommand( () =>
+      {
+         _messageBoxLegacyPattern.Invoke();
+         var handle = _discord.Properties.NativeWindowHandle;
+         using ( new WindowTemporaryForgrounder( handle, true ) )
+         {
+            SendKeys.SendWait( "!toot" );
+            SendKeys.SendWait( "{Enter}" );
+         }
+      } ) );
+   }
+}
--- DiscordAutoDrop/MainWindow.xaml.cs
+++ DiscordAutoDrop/MainWindow.xaml.cs
@@ -1,40 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Windows;
-using System.Windows.Forms;
-using DiscordAutoDrop.MVVM;
-using DiscordAutoDrop.Utilities;
-using FlaUI.Core.AutomationElements.Infrastructure;
-using FlaUI.Core.Patterns;
-
-namespace DiscordAutoDrop
+﻿namespace DiscordAutoDrop
 {
    public partial class MainWindow
    {
-      private readonly AutomationElement _discord;
-      private readonly IInvokePattern _messageBoxLegacyPattern;
-
       public MainWindow()
       {
          InitializeComponent();
-
-         using ( var discordFinder = new DiscordFinder() )
-         {
-            Debug.Assert( discordFinder.Inititialize() );
-            (_discord, _messageBoxLegacyPattern) = discordFinder.FindDiscord();
-         }
-      }
-
-      private void OnButtonClick( object sender, RoutedEventArgs e )
-      {
-         _messageBoxLegacyPattern.Invoke();
-         var handle = _discord.Properties.NativeWindowHandle;
-         using ( new WindowTemporaryForgrounder( handle, true ) )
-         {
-            SendKeys.SendWait( "!toot" );
-            SendKeys.SendWait( "{Enter}" );
-         }
       }
    }
 }
