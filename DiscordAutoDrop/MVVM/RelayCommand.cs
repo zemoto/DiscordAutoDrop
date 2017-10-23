6f6430eb3846c8eaeecfadb3d48f76f1a4f3b9e9
David Markachev <zemoto@gmail.com>
2017-10-23 12:13:52 -0400

    +RelayCommands

---
+++ DiscordAutoDrop/MVVM/RelayCommand.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DiscordAutoDrop.MVVM
+{
+   public class RelayCommand : ICommand
+   {
+      readonly Action _execute;
+      readonly Func<bool> _canExecute;
+
+      public RelayCommand( Action execute, Func<bool> canExecute = null )
+      {
+         _execute = execute ?? throw new ArgumentNullException( nameof( execute ) );
+         _canExecute = canExecute;
+      }
+
+      public bool CanExecute( object parameter )
+      {
+         return _canExecute?.Invoke() ?? true;
+      }
+
+      public event EventHandler CanExecuteChanged
+      {
+         add => CommandManager.RequerySuggested += value;
+         remove => CommandManager.RequerySuggested -= value;
+      }
+
+      public void Execute( object _ )
+      {
+         _execute();
+      }
+   }
+
+   public class RelayCommand<T> : ICommand
+   {
+      readonly Action<T> _execute;
+      readonly Predicate<T> _canExecute;
+
+      public RelayCommand( Action<T> execute, Predicate<T> canExecute = null )
+      {
+         _execute = execute ?? throw new ArgumentNullException( nameof( execute ) );
+         _canExecute = canExecute;
+      }
+
+      public bool CanExecute( object parameter )
+      {
+         if ( _canExecute == null )
+         {
+            return true;
+         }
+         if ( parameter == null && typeof( T ).IsValueType )
+         {
+            return _canExecute( default( T ) );
+         }
+         return _canExecute( (T)parameter );
+      }
+
+      public event EventHandler CanExecuteChanged
+      {
+         add => CommandManager.RequerySuggested += value;
+         remove => CommandManager.RequerySuggested -= value;
+      }
+
+      public void Execute( object parameter )
+      {
+         if ( parameter == null )
+         {
+            parameter = default( T );
+         }
+         _execute( (T)parameter );
+      }
+   }
+}
